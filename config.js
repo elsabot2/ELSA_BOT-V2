7bac97ab4f4c5bb3f35f1b0518f2e0c285425e84
hzhsbsjssushs <136082609+hzhsbsjssushs@users.noreply.github.com>
2024-03-09 20:17:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://mongodbural:mongodbural@cluster0.j0d7xng.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/vajirabot1/KING-VAJIRA-MD'
+global.github = 'h'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://653d199bb6634.site123.me/' // add your username
 global.sudo = process.env.SUDO || '201098906252'
 global.devs = '201098906252';
-global.website = 'https://github.com/vajirabot1/KING-VAJIRA-MD' //wa.me/+25000000000000
+global.website = 'https://chat.whatsapp.com/ItyX14n6vfO3aBHuZ2b8de' //wa.me/+25000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ab4018d27c9929dc12595.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? '*『❄️┇❆ 𝐸𝐿𝑆𝐴 𝐵𝑂𝑇-𝑀𝐷 ❆┇❄️』*' : process.env.BOT_NAME,
@@ -43,7 +43,7 @@ module.exports = {
     API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
-  VERSION: process.env.VERSION === undefined ? 'v1' : process.env.VERSION,
+  VERSION: process.env.VERSION === undefined ? 'v2' : process.env.VERSION,
   LANG: process.env.THEME|| 'ELSA',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
