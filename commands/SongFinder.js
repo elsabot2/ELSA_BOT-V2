8017dc117630f354578fd3f294db3243c393df4c
hzhsbsjssushs <136082609+hzhsbsjssushs@users.noreply.github.com>
2024-03-12 21:58:30 +0200

    Update SongFinder.js

--- commands/SongFinder.js
+++ commands/SongFinder.js
@@ -1,26 +1,26 @@
 const { tlang,shazam,prefix,cmd } = require('../lib')
 let yts = require("secktor-pack");
 cmd({
-        pattern: "find",
+        pattern: "اكشف",
         category: "misc",
         desc: "Finds info about song"
     },
     async(Void, citel, args) => {
         let mime = citel.quoted.mtype
-        if (!citel.quoted) return citel.reply(`Send/Reply Image ${prefix}shaxam`);
-        if (!/audio/.test(mime)) return citel.reply(`Send/Reply audio ${prefix}shazam`);
+        if (!citel.quoted) return citel.reply(`*֎╎رد عـلـي مـقـطـع صـوتـي*`);
+        if (!/audio/.test(mime)) return citel.reply(`*֎╎رد عـلـي مـقـطـع صـوتـي*`);
         let buff = await citel.quoted.download();
         let data = await shazam(buff);
         if (!data.status) return citel.reply(data);
 	let search = await yts(data.title);
 let anu = search.videos[0];
-          let h =  `*Title :* _${data.title}_           
-*Artist :* _${data.artists}_            
-*Album :* _${data.album}_                   
-*Release :* _${data.release_date}
+          let h =  `*❄️⃝🧚‍♀️الـعـنـوان🗒️┇*_${data.title}_           
+*❄️⃝🧚‍♀️الـفـنـان┇* _${data.artists}_            
+*❄️⃝🧚‍♀️الالـبـوم┇* _${data.album}_                   
+*❄️⃝🧚‍♀️تـاريخ الـنشر┇* _${data.release_date}
 
-*To Download Song*
-${prefix}ytmp3 ${anu.url}
+*֎╎لـتـحمـيل الاغـنـيه*
+${prefix}صوتي ${anu.url}
 `
 
 let buttonMessaged = {
